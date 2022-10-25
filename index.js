d76d73248f84b104f76b3a201156e4901328ada3
1Aminul <aminulsc12@gmail.com>
2022-10-25 21:01:27 +0600

    category added

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const express = require('express')
+const app = express()
+const port = 5000
+
+const category = require('./fakedb/catagory.json')
+const course = require('./fakedb/data.json')
+
+app.get('/category',(req, res)=>{
+    res.send(category)
+})
+
+app.get('/courses/:id', (req, res)=>{
+    console.log(req);
+    // res.send(course);
+})
+app.get('/courses/:id',(req, res)=>{
+
+})
+
+
+
+app.listen(port, ()=>{
+    console.log('course api running', port);
+})
